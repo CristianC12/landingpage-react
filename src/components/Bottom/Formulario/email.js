acb155597da08132a3396d915de6dc347ee34ebf
Cristian Martini <cristiamcarlosm@hotmail.com>
2021-11-25 13:45:46 -0300

    Arrumando validação do form

--- src/components/Bottom/Formulario/email.js
+++ src/components/Bottom/Formulario/email.js
@@ -1,13 +1,31 @@
 import emailjs from 'emailjs-com'
 import './style.css'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+ const validationPost = yup.object().shape({
+    name:  yup.string().required("O nome é obrigatório").max(40, "O nome precisa ter menos de 40 caracteres") ,
+    user_email:  yup.string().required("O email é obrigatório").max(50, "O email precisa ter menos de 50 caracteres") ,
+    phone:  yup.string().required("O telefone é obrigatório").max(15, "O telefone precisa ter menos de 15 caracteres"),
+    content: yup.string().required("A mensagem é obrigatória").max(250, "A mensagem precisa ter menos de 250 caracteres")
+}) 
 
 
-const Mailer = () => {
-    function sendEmail(e) {
+function Mailer(){
+
+
+
+    const { register, handleSubmit, formState: {errors} } = useForm({
+        resolver: yupResolver(validationPost)
+    })
+
+    const onSubmit = data => console.log(data);
+ 
+/*      function sendEmail(e) {
         e.preventDefault();
-        alert("Formulário enviado!")
         emailjs.sendForm('service_ef5h6ml', 'template_dio2uoc', e.target, 'user_X0v58AzfU1xoo3U4U14Zu').then(res=>{console.log(res);}).catch(err=> console.log(err));
-    }
+    }   */
     return (
         <>
             <section id="formSection">
@@ -16,7 +34,7 @@ const Mailer = () => {
                     Contato
                 </h1>
                 <div className="formulario">
-                    <form onSubmit={sendEmail} method="POST" className="venda">
+                    <form onSubmit={handleSubmit(onSubmit)} method="POST" className="venda">
                         <div className="formDiv">
                             <label2 className="labelFormulario" for="nomesobrenome">
                                 Nome e sobrenome
@@ -26,51 +44,56 @@ const Mailer = () => {
                                 type="text"
                                 id="nomesobrenome"
                                 className="input-padrao"
-                                maxLength="30"
+                                maxLength="45"
+                                {...register("name")}
                             />
+                            <p className="error-message">{errors.name?.message}</p>
 
                             <label2 className="labelFormulario" for="email">
                                 Email
+                            </label2>
                                 <input
                                     name="user_email"
                                     type="email"
                                     id="email"
                                     className="input-padrao"
-                                    required
                                     placeholder="seuemail@dominio.com"
-                                    maxLength="50"
+                                    maxLength="60"
                                     minLength="2"
+                                    {...register("user_email")}
                                 />
-                            </label2>
+                                <p className="error-message">{errors.user_email?.message}</p>
 
                             <label2 className="labelFormulario" for="telefone">
                                 Telefone
+                                </label2>
                                 <input
                                     name="phone"
                                     type="tel"
                                     id="telefone"
                                     className="input-padrao"
-                                    required
                                     placeholder="(XX) XXXXX-XXXX"
-                                    maxLength="15"
+                                    maxLength="16"
+                                    {...register("phone")}
                                 />
-                            </label2>
+                                <p className="error-message">{errors.phone?.message}</p>
                         </div>
                         <div className="formDiv">
                             <label2 className="labelFormulario">
                                 Mensagem
+                            </label2>
                                 <textarea
-                                    name="message"
+                                    name="content"
                                     cols="70"
                                     rows="6"
                                     className="input-padrao"
                                     id="mensagem "
-                                    required
-                                    maxLength="250"
+                                    maxLength="251"
+                                    {...register("content")}
                                 />
-                            </label2>
+                            <p className="error-message">{errors.content?.message}</p>
                         </div>
-                        <input type="submit" event="preventDefault" value="Enviar" className="enviar" />
+                        <input type="submit" value="Enviar" className="enviar" />
                     </form>
                 </div>
             </section>
